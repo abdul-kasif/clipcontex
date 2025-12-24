f4d43838247973591810f36c558e5b34cd6fc76c
abdul-kasif <abdulkasif.ra@gmail.com>
2025-12-24 22:14:04 +0530

    feat(auto_tags): add app based tags generation functionality

--- src-tauri/src/context.rs
+++ src-tauri/src/context.rs
@@ -1,5 +1,5 @@
 mod app_info;
-mod auto_tag;
+mod auto_tags;
 mod normalize_app_name;
 
 #[cfg(target_os = "linux")]
@@ -9,5 +9,5 @@ mod linux;
 mod tests;
 
 pub use app_info::{get_active_app_info, AppInfo};
-pub use auto_tag::generate_auto_tags;
+pub use auto_tags::generate_auto_tags;
 pub use normalize_app_name::normalize_app_class;
---
+++ src-tauri/src/context/auto_tags.rs
@@ -0,0 +1,18 @@
+// ===== Imports =====
+use std::collections::HashSet;
+
+// ===== Modules =====
+mod app_based;
+
+// ===== Public API =====
+pub fn generate_auto_tags(content: &str, app_class: Option<&str>) -> Vec<String> {
+    let mut tags = HashSet::new();
+
+    if let Some(app_class) = app_class {
+       app_based::generate_app_based_tags(app_class, &mut tags);
+    }
+
+    let mut tags_vec: Vec<_> = tags.into_iter().collect();
+    tags_vec.sort();
+    tags_vec
+}
