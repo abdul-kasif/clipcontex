fc97670566a712b8a0fa2e5d5519f9ee167dc49b
abdul-kasif <abdulkasif.ra@gmail.com>
2026-01-05 23:15:35 +0530

    feat: add system tray and global shortcut in module

--- src-tauri/src/core.rs
+++ src-tauri/src/core.rs
@@ -1,2 +1,4 @@
+pub mod global_shortcut;
 pub mod platform;
+pub mod system_tray;
 pub mod window_creation;
--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,2 +1,6 @@
+pub mod clipboard;
+pub mod commands;
+pub mod config;
+pub mod context;
 pub mod core;
-
+pub mod storage;
