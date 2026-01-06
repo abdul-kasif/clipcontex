88713136ebbec08b85d0f69d220905506abc22cb
abdul-kasif <abdulkasif.ra@gmail.com>
2026-01-06 16:21:30 +0530

    feat: initialize all modules and plugins, implement linux based enviromental variables

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -4,3 +4,65 @@ pub mod config;
 pub mod context;
 pub mod core;
 pub mod storage;
+
+use crate::core::setup::setup;
+use tauri_plugin_autostart::MacosLauncher;
+
+#[cfg(target_os = "linux")]
+use tikv_jemallocator::Jemalloc;
+
+#[cfg(target_os = "linux")]
+#[global_allocator]
+static GLOBAL_ALLOCATOR: Jemalloc = Jemalloc;
+
+#[cfg_attr(mobile, tauri::mobile_entry_point)]
+pub fn run() {
+    #[cfg(target_os = "linux")]
+    {
+        std::env::set_var(
+            "MALLOC_CONF",
+            "dirty_decay_ms:1000,muzzy_decay_ms:1000,background_thread:true",
+        );
+
+        std::env::set_var("WEBKIT_DISABLE_COMPOSITING_MODE", "1");
+        std::env::set_var("WEBKIT_DISABLE_WEBGL", "1");
+        std::env::set_var("WEBKIT_DISABLE_MEDIA_SOURCE", "1");
+        std::env::set_var("WEBKIT_DISABLE_CACHE", "1");
+        std::env::set_var("WEBKIT_DISABLE_WEB_PROCESS_CACHE", "1");
+    }
+
+    #[cfg(all(target_os = "linux", debug_assertions))]
+    {
+        std::env::set_var("G_DEBUG", "gc-friendly");
+        std::env::set_var("G_SLICE", "always-malloc");
+    }
+
+    tracing_subscriber::fmt()
+        .with_target(false)
+        .without_time()
+        .compact()
+        .init();
+
+    tauri::Builder::default()
+        .plugin(tauri_plugin_opener::init())
+        .plugin(tauri_plugin_clipboard_manager::init())
+        .plugin(tauri_plugin_os::init())
+        .plugin(tauri_plugin_autostart::init(
+            MacosLauncher::LaunchAgent,
+            Some(vec![]),
+        ))
+        .setup(|app| setup(app))
+        .invoke_handler(tauri::generate_handler![
+            commands::get_recent_clips,
+            commands::pin_clip,
+            commands::delete_clip,
+            commands::clear_history,
+            commands::load_config,
+            commands::save_config,
+            commands::complete_onboarding,
+            commands::ignore_next_clipboard_update,
+            commands::is_kdotool_installed,
+        ])
+        .run(tauri::generate_context!())
+        .expect("Error running Tauri application");
+}
