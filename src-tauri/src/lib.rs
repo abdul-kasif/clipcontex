9c3dda8b43b14f5de28ea6ac83ed18df8683e948
abdul-kasif <abdulkasif.ra@gmail.com>
2026-01-06 22:07:49 +0530

    feat: installed tauri_plugin_store to store persistant data in local

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -53,6 +53,7 @@ pub fn run() {
 
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
+        .plugin(tauri_plugin_store::Builder::default().build())
         .plugin(tauri_plugin_clipboard_manager::init())
         .plugin(tauri_plugin_os::init())
         .plugin(tauri_plugin_autostart::init(
