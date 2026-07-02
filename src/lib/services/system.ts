2faf8431e6f1a1172251b1e0b2b2da6193b275e4
Abdul Kasif <abdulkasif.ra@gmail.com>
2026-07-02 17:27:51 +0530

    fix(context): Cleanup kdotool implementation in frontend
    Remove `check_kdotool_installed()` function from system.ts and the
    component that was used to show when the `kdotool` is not installed is
    also removed from +page.svelte
    
    Signed-off-by: Abdul Kasif <abdulkasif.ra@gmail.com>

--- src/lib/services/system.ts
+++ src/lib/services/system.ts
@@ -1,10 +1,5 @@
 import { invoke } from "@tauri-apps/api/core";
 
-export async function checkKdotoolInstalled(): Promise<boolean> {
-  const response = await invoke("check_kdotool_installed");
-  return response === true;
-}
-
 export async function setDragging(isDragging: boolean): Promise<void> {
   await invoke("set_dragging", { isDragging });
 }
